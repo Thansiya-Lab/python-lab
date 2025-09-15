7e168605254b0078d63bef3790250c94fca30c6c
Thansiya-Lab <thansiyanasrina@gmail.com>
2025-09-15 15:47:23 +0530

    Add files via upload

---
+++ pos ,neg and zero.py
@@ -0,0 +1,7 @@
+n=int(input("Enter the value for n"))
+if(n>0):
+    print("The given number is positive")
+elif(n<0):
+    print("The given number is negative")
+else:
+    print("The number is either positive nor negative")
