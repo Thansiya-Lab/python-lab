e7b653e2fada7a5ddcd42eeeebf0b30a9c49d5b1
Thansiya-Lab <thansiyanasrina@gmail.com>
2025-09-15 15:29:45 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,11 @@
+print("LARGEST AMONG THE 3 NUMBERS")
+a=int(input("enter the 1st number"))
+b=int(input("enter the 2nd number"))
+c=int(input("enter the 3rd number"))
+if (a>b and a>c):
+    print("largest among the number is",a)
+elif(b>a and b>c):
+    print("largest among the number is",b)
+else:
+    print ("largest among the number is",c)
+          
